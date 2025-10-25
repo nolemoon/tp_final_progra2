34e803cd48243393128ff7ddb1c43668ffd4de4d
renatag7 <renigarabito@gmail.com>
2025-10-24 22:01:03 -0300

    Creacion clase Usuario e interfaz ABM

---
+++ src/Usuario.java
@@ -0,0 +1,82 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public abstract class Usuario implements ABM<Usuario> {
+    private String nombre;
+    private String email;
+    private String telefono;
+    private boolean usuarioActivo;
+    private final LocalDateTime fechaRegistro;
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yy HH:mm:ss");
+
+    public Usuario(String nombre, String email, String telefono) {
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+        this.usuarioActivo = true;
+        this.fechaRegistro = LocalDateTime.now();
+    }
+
+    public Usuario() {
+        this.usuarioActivo = true;
+        this.fechaRegistro = LocalDateTime.now();
+    }
+
+    public LocalDateTime getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public boolean isUsuarioActivo() {
+        return usuarioActivo;
+    }
+
+    public void setUsuarioActivo(boolean usuarioActivo) {
+        this.usuarioActivo = usuarioActivo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public abstract boolean alta(Usuario obj);
+
+    @Override
+    public abstract Usuario baja(int id);
+
+    @Override
+    public abstract boolean modificar(Usuario obj);
+
+
+    @Override
+    public String toString() {
+        return "Usuario {" +
+                "nombre='" + nombre + '\'' +
+                ", email='" + email + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", cuenta activa=" + usuarioActivo +
+                ", fecha de registro=" + fechaRegistro.format(formatter) +
+                '}';
+    }
+}
