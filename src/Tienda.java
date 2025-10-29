1d7afd7ec21f98f1746eba9b068de9671b678c8c
Noel Solange Montes Dupuy <nmontesdupuy@gmail.com>
2025-10-29 02:30:16 -0300

    actualizo tienda

--- src/Tienda.java
+++ src/Tienda.java
@@ -4,10 +4,22 @@ public class Tienda {
 
 
     public void iniciar()
-    {   Scanner sc =new Scanner();
-        System.out.println(" Ingresar como: \n1)USUARIO\n2)ADMINISTRADOR");
-        int opcion = sc.nextInt();
+    {   Scanner sc =new Scanner(System.in);
+        System.out.println(" Ingresar como: \n1)CLIENTE\n2)ADMINISTRADOR");
+        int tipoUsuario = sc.nextInt();
 
+        System.out.println("1)Ya tengo cuenta\n2)Registrarme");
+        int opcionIngreso = sc.nextInt();
 
+        switch(opcionIngreso)
+        {
+            case 1:
+                System.out.println(" Ingrese su mail: ");
+                String mail = sc.nextLine();
+                System.out.println("Ingrese su clave: ");
+                String pass = sc.nextLine();
+                //if(tipoUsuario==1){ buscarenlalista}
+        }   case 2:
+               // if(tipoUsuario==1){alta de cliente}else{alta de administrador}
     }
 }
