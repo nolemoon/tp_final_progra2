350f46cec8f7bfc94eda599f0b06c8a94f145543
Noel Solange Montes Dupuy <nmontesdupuy@gmail.com>
2025-10-29 01:49:52 -0300

    iniciar tienda

--- src/Tienda.java
+++ src/Tienda.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class Tienda {
 
+
+    public void iniciar()
+    {   Scanner sc =new Scanner();
+        System.out.println(" Ingresar como: \n1)USUARIO\n2)ADMINISTRADOR");
+        int opcion = sc.nextInt();
+
+
+    }
 }
