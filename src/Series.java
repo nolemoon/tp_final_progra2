71fbbfe68791d312296cfd8041cced493e7ab9af
NewArkantos2 <agus.rey.01@gmail.com>
2025-11-11 23:26:04 -0300

    prueba xd

---
+++ src/Series.java
@@ -0,0 +1,44 @@
+public final class Series extends AbstractProducto{
+
+    // Atributos
+    private int temporadas;
+    private int capitulos;
+
+    // Constructores
+    // TODO: añadir llamado al constructor super + sus atributos.
+    public Series() {
+    }
+
+    public Series(int temporadas, int capitulos) {
+        this.temporadas = temporadas;
+        this.capitulos = capitulos;
+    }
+
+    // Getter y Setter
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public int getCapitulos() {
+        return capitulos;
+    }
+
+    public void setCapitulos(int capitulos) {
+        this.capitulos = capitulos;
+    }
+
+    // To String
+    // TODO: Invocar al Super To String
+    @Override
+    public String toString() {
+        return "Series{" +
+                "temporadas=" + temporadas +
+                ", capitulos=" + capitulos +
+                '}';
+    }
+
+}
