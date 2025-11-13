ac960af2cf8b8427b84f6a87da132526ae1cb46b
Noel Solange Montes Dupuy <nmontesdupuy@gmail.com>
2025-11-13 17:21:24 -0300

    actualizo tienda

--- src/Tienda.java
+++ src/Tienda.java
@@ -1,25 +1,69 @@
+import Exceptions.UsuarioNoEncontradoException;
+
 import java.util.Scanner;
 
 public class Tienda {
 
+    Scanner sc = new Scanner(System.in);
 
-    public void iniciar()
-    {   Scanner sc =new Scanner(System.in);
+    public void iniciar() {
         System.out.println(" Ingresar como: \n1)CLIENTE\n2)ADMINISTRADOR");
         int tipoUsuario = sc.nextInt();
-
+        sc.nextLine();
         System.out.println("1)Ya tengo cuenta\n2)Registrarme");
         int opcionIngreso = sc.nextInt();
+        sc.nextLine();
 
-        switch(opcionIngreso)
-        {
+        switch (opcionIngreso) {
             case 1:
-                System.out.println(" Ingrese su mail: ");
-                String mail = sc.nextLine();
-                System.out.println("Ingrese su clave: ");
-                String pass = sc.nextLine();
-                //if(tipoUsuario==1){ buscarenlalista}
-        }   case 2:
-               // if(tipoUsuario==1){alta de cliente}else{alta de administrador}
+                ingresar();
+                break;
+            //if(tipoUsuario==1){ buscarenlalista}
+            case 2:
+                registrarse();
+                break;
+            default:
+                System.out.println("Opcion invalida");
+        }
     }
+
+    public void registrarse() {
+        String email;
+        String contrasenia;
+        String nombre;
+        String telefono;
+
+        System.out.println("Ingrese su e-mail");
+        email = sc.nextLine();
+        System.out.println("Ingrese su contraseña");
+        contrasenia = sc.nextLine();
+        System.out.println("Ingrese su nombre");
+        nombre = sc.nextLine();
+        System.out.println("Ingrese su telefono");
+        telefono = sc.nextLine();
+        Usuario aux = new Usuario(nombre, email, telefono, contrasenia);
+
+        Usuario.listaUsuarios.add(aux);
+    }
+
+    public void ingresar() throws UsuarioNoEncontradoException {
+        String email;
+        String contrasenia;
+
+        System.out.println("Ingrese su e-mail");
+        email = sc.nextLine();
+        System.out.println("Ingrese su contraseña");
+        contrasenia = sc.nextLine();
+
+        for (Usuario aux : listaUsuarios) {
+            if (aux.getEmail().equals(email) && aux.getContrasenia().equals(contrasenia)) {
+                System.out.println("Bienvenido" + aux.getNombre);
+                mostrarmenu
+            } else throw new UsuarioNoEncontradoException("No se encontró usuario con los datos ingresados.");
+        }
+
+
+    }
+
 }
+
