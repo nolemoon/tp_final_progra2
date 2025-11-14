90cc0a2f2f056431a0835f848c71a0f10f6cfc6c
renatag7 <renigarabito@gmail.com>
2025-11-14 02:28:35 -0300

    serie documentada

--- src/Productos/Series.java
+++ src/Productos/Series.java
@@ -4,17 +4,38 @@ package Productos;
 import Enum.Genero;
 import Enum.Suscripcion;
 
+/**
+ * Clase final que representa una serie, extiende de la clase abstracta
+ * {@code Producto}. Agrega como atributos específicos temporadas y capitulos.
+ * Es decir, contiene toda la info. general heredada de Producto, sumando datos propios.
+ * @author Agustino
+ */
 public final class Series extends Producto{
 
-    // Atributos
+
     private int temporadas;
     private int capitulos;
 
-    // Constructores
+    /**
+     * Constructor que llama al super() vacío.
+     */
     public Series() {
         super();
     }
 
+    /**
+     * Constructor que inicializa una serie con todos sus atributos.
+     *
+     * @param nombre nombre de la serie
+     * @param genero género al que pertenece
+     * @param precio precio de compra o alquiler
+     * @param anioPublicado año en el que comenzó a emitir
+     * @param creador creador de la serie
+     * @param descripcion sinopsis de la serie
+     * @param tipoSuscripcion suscripción para acceder
+     * @param temporadas cantidad total de temporadas
+     * @param capitulos cantidad total de capítulos
+     */
     public Series(String nombre, Genero genero, double precio, int anioPublicado, String creador, String descripcion,
                   Suscripcion tipoSuscripcion, int temporadas, int capitulos) {
         super(nombre, genero, precio, anioPublicado, creador, descripcion, tipoSuscripcion);
